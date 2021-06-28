beceab7b57434fe595cfbab5feda07528e5c5808
Nicolas Settembrini <nsettembrini@gmail.com>
2021-06-28 15:12:08 -0300

    Routers, authentication, jwt token

---
+++ blog/routers/blog.py
@@ -0,0 +1,38 @@
+from typing import List
+from fastapi import APIRouter, Depends, status, Response, HTTPException
+from .. import schemas, database, models, oauth2
+from sqlalchemy.orm import Session
+from ..repository import blog 
+
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blogs']
+)
+
+get_db = database.get_db
+
+
+@router.get('/', response_model=List[schemas.ShowBlog])
+def all(db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.get_all(db)
+
+
+@router.post('/', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.create(request, db)
+
+
+@router.delete('/{id}', status_code=status.HTTP_200_OK)
+def delete(id, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.delete(id, db)
+
+
+@router.put('/{id}', status_code=status.HTTP_200_OK)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.update(id, request, db)
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog)
+def show(id, response: Response, db: Session = Depends(get_db), current_user: schemas.User = Depends(oauth2.get_current_user)):
+    return blog.get_id(db)
